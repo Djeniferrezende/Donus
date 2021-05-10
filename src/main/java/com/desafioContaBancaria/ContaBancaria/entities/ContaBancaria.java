7462d4dd9a437d6ca20db99f71b63362dd2581e3
Djeniferrezende <djenifer.rezende@gmail.com>
2021-05-10 20:49:18 -0300

    entity criado

---
+++ src/main/java/com/desafioContaBancaria/ContaBancaria/entities/ContaBancaria.java
@@ -0,0 +1,70 @@
+package com.desafioContaBancaria.ContaBancaria.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ContaBancaria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private Integer numeroConta;
+    private Double saldo;
+
+    public ContaBancaria(){
+
+    }
+
+    public ContaBancaria(Long id, String nome, String cpf, Integer numeroConta, Double saldo) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(Integer numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
