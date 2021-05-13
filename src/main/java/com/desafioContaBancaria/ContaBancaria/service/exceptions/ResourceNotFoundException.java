0b452adbdb19d25fe1d089d7f68019577421df9b
Djeniferrezende <djenifer.rezende@gmail.com>
2021-05-13 10:15:35 -0300

    atualização de mensagem da excessão

--- src/main/java/com/desafioContaBancaria/ContaBancaria/service/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/desafioContaBancaria/ContaBancaria/service/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package com.desafioContaBancaria.ContaBancaria.service.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(Object id){
         super("Resource not found. Id " + id );
     }
