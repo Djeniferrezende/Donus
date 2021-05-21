d1296817265a4c95a9efc24c8698611a0e5a2fd5
Djeniferrezende <djenifer.rezende@gmail.com>
2021-05-21 16:02:14 -0300

    implantação de documentação com swagger

--- src/main/java/com/desafioContaBancaria/ContaBancaria/resource/ContaBancariaResource.java
+++ src/main/java/com/desafioContaBancaria/ContaBancaria/resource/ContaBancariaResource.java
@@ -4,6 +4,8 @@ import com.desafioContaBancaria.ContaBancaria.entities.ContaBancaria;
 import com.desafioContaBancaria.ContaBancaria.entities.Deposito;
 import com.desafioContaBancaria.ContaBancaria.entities.Transferencia;
 import com.desafioContaBancaria.ContaBancaria.service.ContaBancariaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/conta")
+@Api(value="Api Rest Conta Bancaria")
+//qualquer dominio pode acessar esse dominio
+@CrossOrigin(origins = "*")
 public class ContaBancariaResource {
 
     @Autowired
@@ -19,12 +24,14 @@ public class ContaBancariaResource {
 
 
     @GetMapping
+    @ApiOperation(value ="Retorna lista de contas")
     public ResponseEntity<List<ContaBancaria>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
     //buscar por id
     @GetMapping("/{id}")
+    @ApiOperation(value ="Retorna produto unico")
     public ResponseEntity<ContaBancaria> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
 
@@ -32,6 +39,7 @@ public class ContaBancariaResource {
 
     //deletar usuario
     @DeleteMapping(value = "/{id}")
+    @ApiOperation(value ="Deleta por id")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         service.deletar(id);
         return ResponseEntity.noContent().build();
@@ -39,12 +47,14 @@ public class ContaBancariaResource {
 
 
     @PostMapping
+    @ApiOperation(value ="Salva conta")
     public ResponseEntity<ContaBancaria> salvar(@RequestBody ContaBancaria conta) {
 
         return ResponseEntity.ok().body(service.criarNovaConta(conta));
     }
 
     @PostMapping("{id}/novo-deposito")
+    @ApiOperation(value ="faz deposito na conta bancaria")
     public ResponseEntity<ContaBancaria> depositar(@PathVariable Long id, @RequestBody Deposito deposito) {
         ContaBancaria conta = service.findById(id);
         service.novoDeposito(conta, deposito);
@@ -52,11 +62,13 @@ public class ContaBancariaResource {
     }
 
     @GetMapping("{id}/depositos")
+    @ApiOperation(value ="Retorna lista de depositos")
     public ResponseEntity<List<Deposito>> depositos(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findDepositosByConta(id));
     }
 
     @PostMapping("{idOrigem}/nova-transferencia/{idDestino}")
+    @ApiOperation(value ="faz uma transferencia")
     public ResponseEntity<Transferencia> transferir(@PathVariable Long idOrigem,@PathVariable Long idDestino, @RequestBody Transferencia transferencia) {
         ContaBancaria contaOrigem = service.findById(idOrigem);
         ContaBancaria contaDestino = service.findById(idDestino);
